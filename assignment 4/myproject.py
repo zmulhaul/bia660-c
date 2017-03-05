c9241a26f9cbcbdfb24e561542e3ab6ce8bda76f
Zachary <zmulhaul@stevens.edu>
2017-03-05 17:56:04 -0500

    Project

--- assignment 4/myproject.py
+++ assignment 4/myproject.py
@@ -4,17 +4,23 @@ import random
 import requests
 import time
 
+
 from flask import Flask, request, Response
+
+# not allowing for server to exceed max tries - stackoverflow assisted
+from requests.adapters import HTTPAdapter
+s = requests.Session()
+s.mount('http://slack.com', HTTPAdapter(max_retries=5))
 
 
 application = Flask(__name__)
 
 # FILL THESE IN WITH YOUR INFO
-my_bot_name = '' #e.g. zac_bot
-my_slack_username = '' #e.g. zac.wentzell
+my_bot_name = 'zachgraysonbot' #e.g. zac_bot
+my_slack_username = 'zacharymulhaul' #e.g. zac.wentzell
 
 
-slack_inbound_url = ''
+slack_inbound_url = 'https://hooks.slack.com/services/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'
 
 
 # this handles POST requests sent to your server at SERVERIP:41953/slack
@@ -24,6 +30,7 @@ def inbound():
     # This will randomly choose a value between 0 and 10 using a uniform distribution.
     delay = random.uniform(0, 10)
     time.sleep(delay)
+    req = request
 
     print '========POST REQUEST @ /slack========='
     response = {'username': my_bot_name, 'icon_emoji': ':robot_face:', 'text': ''}
@@ -36,19 +43,32 @@ def inbound():
     inbound_message = username + " in " + channel + " says: " + text
     print '\n\nMessage:\n' + inbound_message
 
-    if username in [my_slack_username, 'zac.wentzell']:
-        # Your code for the assignment must stay within this if statement
-
-        # A sample response:
-        if text == "What's your favorite color?":
-        # you can use print statments to debug your code
-            print 'Bot is responding to favorite color question'
-            response['text'] = 'Blue! (Waited for {}s before responding)'.format(delay)
-            print 'Response text set correctly'
+    if username in [my_slack_username, 'zacharymulhaul'] or username == 'zac.wentzell':
+            text == "<BOTS_RESPOND>"
+            response['text'] = 'Hello, my name is zachgraysonbot. I belong to zacharymulhaul. I live at 41953/slack'
             print response['text']
 
+    if username in [my_slack_username, 'zacharymulhaul'] or username == 'zac.wentzell':
+            text == "<I_NEED_HELP_WITH CODING>:"
+            # need to configure integrating =title value in text
 
-        if slack_inbound_url and response['text']:
+
+            #Slack API component for Questions, etc.
+            base_url = 'https://api.stackexchange.com/2.2/search/advanced?order=desc&sort=activity&answers&'
+            complete_url = 'base_url + title + =5&site=stackoverflow'
+
+            get_url = requests.get(complete_url).json()
+            print(get_url)
+
+            for item in get_url['items']:
+                print item['title'],['link'],['creation_date']
+
+
+
+
+
+
+    if slack_inbound_url and response['text']:
             r = requests.post(slack_inbound_url, json=response)
 
     print '========REQUEST HANDLING COMPLETE========\n\n'
